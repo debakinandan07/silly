a268197ae5deb2978f1cca493343fff8f3a8d7e6
Debakinandan Panigrahi <debakipanigrahi107@gmail.com>
2026-01-17 19:31:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// app.post("/submit", (req, res) => {
+//   res.send();
+// });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
